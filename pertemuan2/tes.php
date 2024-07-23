811b4d65db140d4d0620982710d50b4bdd2691a6
Aqueit <alghifarimadam04@gmail.com>
2024-07-23 09:00:57 +0800

    pertemuan 2

---
+++ pertemuan2/tes.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Javascript</title>
+    <script>
+        alert("Selamat datang di tutorial belajar Javascript");
+        function sayHello() {
+            alert("Hello World");
+        }
+    </script>
+</head>
+
+<body>
+    <button onclick="sayHello()">Klik Aku!</button>
+
+</body>
+
+</html>
