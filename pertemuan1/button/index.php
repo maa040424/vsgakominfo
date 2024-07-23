bda8d4a3d2ef5f0e1fac9b76d8be4c1bf5e483c1
Aqueit <alghifarimadam04@gmail.com>
2024-07-23 08:26:16 +0800

    pertemuan1

---
+++ pertemuan1/button/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css"
+    <title>Pertemuan 1</title>
+</head>
+<body>
+    <h3>Pilih Jenis Kelamin</h3>
+
+<form>
+    <input type="radio" name="gender" value="Laki-Laki" > Laki-Laki  <br>
+    <input type="radio" name="gender" value="Perempuan"> Perempuan  <br>
+    <input type="radio" name="gender" value="Lainnya" >lainnya 
+    <br>
+    <input type="submit" value="Submit">
+</form>
+<br>
+<br>
+
+<h3>Pilih Jenis Kelamin</h3>
+
+<form >
+    <input type="button" value="Laki-Laki"> <br>
+    <input type="button" value="Perempuan"> <br>
+</form>
+
+<br>
+<br>
+
+<form>
+    <input type="checkbox" name="gender" value="Laki-Laki" > Laki-Laki  <br>
+    <input type="checkbox" name="gender" value="Perempuan"> Perempuan  <br>
+    <input type="checkbox" name="gender" value="Lainnya" >lainnya 
+    <br>
+    <input type="submit" value="Submit">
+</form>
+<br>
+<h3>pilih jenis kelamin</h3>
+<form >
+    <select  id="gender">
+        <option value="1">Laki-Laki</option>
+        <option value="2">Perempuan</option>
+    </select>
+</form>
+
+<script src="js/bootstrap.mi.js" ></script>
+
+
+
+
+</body>
+</html>
